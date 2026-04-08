33ce74dadacb0094ecfd5ee77433fde486b39a04
arfrhmn21 <139621451+arfrhmn21@users.noreply.github.com>
2026-04-08 08:21:54 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Hai, " << i * (i + 1) << "!" << endl;
+    }
+
+    return 0;
+}
